0d8b468f34db36dec870fc7f55bce2f5d22be534
Manuel Tomas Teves <manuel.tomas.teves@gmail.com>
2025-07-14 21:24:57 -0300

    Setup firmware

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,4 +1,5 @@
-#include "BluetoothSerial.h"
+#include <Arduino.h>
+#include <BluetoothSerial.h>
 
 #if ( !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED) )
     #error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
@@ -18,10 +19,10 @@
         
 char order = 0;
 BluetoothSerial SerialBT;
+void rotateStepper(int stepPin, int directionPin, int enablePin, bool direction); 
 
 void setup()
 {
-    Serial.begin(115200);
     SerialBT.begin("CASA-DOMOTICA");   //Bluetooth device name is "CASA-DOMOTICA"
 
     pinMode(motor_door_step, OUTPUT);
@@ -40,7 +41,6 @@ void setup()
     digitalWrite(light2, HIGH);
     digitalWrite(light3, HIGH);
     digitalWrite(light4, HIGH);
-    Serial.println("Setup completed");
 }
 
 void loop()
