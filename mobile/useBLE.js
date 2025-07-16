d1298fc1cc3df2cae6e5e14acbe5981d88d2b229
Manuel Teves <manuel.tomas.teves@gmail.com>
2025-07-16 15:19:29 -0300

    security verify

--- mobile/useBLE.js
+++ mobile/useBLE.js
@@ -5,6 +5,7 @@ import { BleManager } from "react-native-ble-plx";
 function useBLE() {
     const bleManager = useMemo(() => new BleManager(), []);
     const [allDevices, setAllDevices] = useState([]);
+    const [scanning, setScanning] = useState(false);
     
     const requestAndroidPermissions = async () => {
         const bluetoothScanPermission = await PermissionsAndroid.request(
@@ -43,32 +44,58 @@ function useBLE() {
             return androidPermissions;
         }
         return true;
-    }
+    };
 
     const isDuplicateDevice = (devices, nextDevice) => devices.findIndex((device) => nextDevice.id === device.id) > -1;
 
-    const scanPeripherals = () => {
+    const checkPermissions = async() => {
+        const scanGranted = await PermissionsAndroid.check(PermissionsAndroid.PERMISSIONS.BLUETOOTH_SCAN);
+        const connectGranted = await PermissionsAndroid.check(PermissionsAndroid.PERMISSIONS.BLUETOOTH_CONNECT);
+        const locationGranted = await PermissionsAndroid.check(PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION);
 
-        console.log("[i] Start scanning...");
-        bleManager.startDeviceScan(null, null, (error, device) => {
-            console.log("[i] Scanning return something...");
-            if(error){
-                console.error("[!]Error at devices scan: ", error);
-            }
-            else if(device) {
-                setAllDevices((prevState) => {
-                    console.log("[i] Device scanned:");
-                    console.log("\t\tName: ", device.name);
-                    console.log("\t\tId: ", device.id);
-                    console.log("\t\tisConnectable: ", device.isConnectable);
-                    console.log("\t\tlocalName: ", device.localName);
-                    if(!isDuplicateDevice(prevState, device)) {
-                        return [...prevState, device];
-                    }
-                    return prevState;
-                })
-            }
-        });
+        // console.log("[i] Verificando permisos existentes:");
+        // console.log("BLUETOOTH_SCAN:", scanGranted);
+        // console.log("BLUETOOTH_CONNECT:", connectGranted);
+        // console.log("ACCESS_FINE_LOCATION:", locationGranted);
+
+        return scanGranted && connectGranted && locationGranted;
+    };
+
+    const scanPeripherals = () => {
+        if(!scanning) {
+            checkPermissions().then((checked) => {
+                if(checked) {
+                    console.log("[i] Start scanning...");
+                    setScanning(true);
+                    bleManager.startDeviceScan(null, { allowDuplicates: false }, (error, device) => {
+                        console.log("[i] Scanning return something...");
+                        if(error){
+                            console.error("[!]Error at devices scan: ", error);
+                        }
+                        else if(device) {
+                            setAllDevices((prevState) => {
+                                console.log("[i] Device scanned:");
+                                console.log("\t\tName: ", device.name);
+                                console.log("\t\tId: ", device.id);
+                                console.log("\t\tisConnectable: ", device.isConnectable);
+                                console.log("\t\tlocalName: ", device.localName);
+                                console.log("\t\tmanufacturerData: ", device.manufacturerData);
+                                if(!isDuplicateDevice(prevState, device)) {
+                                    return [...prevState, device];
+                                }
+                                return prevState;
+                            })
+                        }
+                    });
+                }
+                else {
+                    console.error("[!]Error: No has permissions.");
+                }
+            })
+        }
+        else {
+            console.log("[i] It is already scanning...");
+        }
     };
 
     return ({
